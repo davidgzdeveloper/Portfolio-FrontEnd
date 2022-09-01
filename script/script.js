08e8f334235daa8a25250dae670ceb2d77d5ceb7
davidgzdeveloper <davidgzdeveloper@gmail.com>
2022-08-31 23:13:06 -0300

    mi cuarto commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar-dark');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
